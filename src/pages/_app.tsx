15b0832eabbd3b0d582ccaa782de4711b83046dd
Suhrobbek Soatov <svkhrobbeck@gmail.com>
2025-07-03 16:54:47 +0500

    feat: :sparkles: change library version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 
-import type { AppProps } from "next/app";
 import { Toaster } from "sonner";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
