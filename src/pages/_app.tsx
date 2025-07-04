02579a3ba29bed121f68e387c45f5cb93dbf966b
Suhrobbek Soatov <114657195+svkhrobbeck@users.noreply.github.com>
2025-07-04 17:36:13 +0500

    change toaster options

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Toaster position="top-right" />
+      <Toaster position="top-right" theme="dark" richColors duration={5000} visibleToasts={6}  />
       <Component {...pageProps} />
     </>
   );
